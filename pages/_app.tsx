81178f33a8c8707fe9da28e9be73fc73806d6c0f
stylishjoker <animetplink@gmail.com>
2023-06-04 21:02:58 +0700

    done part 1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,10 @@ import type { AppProps } from "next/app";
 import Layout from "@/components/layout";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const AnyComponent = Component as any;
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnyComponent {...pageProps} />
     </Layout>
   );
 }
